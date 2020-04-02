1f5dd007dd0b12019f2a53e73d79857ccfd503ff
millerb08 <miller.steffens@gmail.com>
2020-04-02 18:12:21 -0500

    added read projects and trasks functions

--- inc/functions.php
+++ inc/functions.php
@@ -10,3 +10,31 @@ function get_project_list(){
     return array();
   }
 }
+
+function get_task_list(){
+  include "connection.php";
+  
+  $sql = "SELECT tasks.*, projects.title as project FROM tasks JOIN projects ON tasks.project_id = projects.project_id";
+  
+  try{
+  return $db->query($sql);
+  }catch (Exception $e){
+    echo "Error: ".$e->getMessage() ."</br>";
+    return array();
+  }
+}
+
+function add_project($title,$category){
+  include "connection.php";
+  $sql = "INSERT INTO projects(title, category) VALUES(?,?)";
+  try{
+    $results = $db->prepare($sql);
+    $results->bindValue(1,$title,PDO::PARAM_STR);
+    $results->bindValue(2,$category,PDO::PARAM_STR);
+    $results->execute();
+  }catch (Exception $e){
+    echo "Error:".$e->getMessage()."<br/>";
+    return false;
+  }
+  return true;
+}
